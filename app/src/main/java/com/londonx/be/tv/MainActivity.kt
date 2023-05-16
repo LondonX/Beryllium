31ad6a9b3b28ce6f4b93adfac259b51610709d05
Mrudula <mrudula@pop-os.localdomain>
2023-05-16 22:14:40 +0530

    Android TV Leanback Version  along with basic icon & banner

--- app/src/main/java/com/londonx/be/tv/MainActivity.kt
+++ app/src/main/java/com/londonx/be/tv/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.github.salomonbrys.kotson.fromJson
@@ -82,6 +83,8 @@ class MainActivity : AppCompatActivity(), AnalyticsListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         playerView.player = player
         player.addAnalyticsListener(this)
